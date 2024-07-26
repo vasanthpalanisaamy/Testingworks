87db62fb1491a879458bd67d599fb5cdf53dd20b
vasanthpalanisaamy <vasanthpalanisaamy@gmail.com>
2024-07-26 13:04:48 +0530

    Add files via upload
    Week1

---
+++ week1/LearnOperators.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class LearnOperators {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=5;
+	
+		System.out.println(a+b);
+
+	}
+
+}
---
+++ week1/day2/LearnIfElse.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class LearnIfElse {
+
+	public static void main(String[] args) {
+		
+		int num=1;
+		
+		//if->ctrl+space => choose the 2nd option
+		
+		if (num>1) {
+			
+			System.out.println("Number is positive");
+			
+		}else {
+			System.out.println("number is negative");
+			
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
